0af1e83e3577ab3727c6deb3208efa387a3740b0
Alexandre Queiroz <dev.alexandrequeiroz@gmail.com>
2021-12-27 08:57:37 -0300

    tipo logradouro

--- src/app/pages/cadastros/tipo-logradouro/tipo-lograduro.module.ts
+++ src/app/pages/cadastros/tipo-logradouro/tipo-lograduro.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { TipoLogradouroComponent } from './tipo-logradouro.component';
 import { TipoLogradouroRoutingModule } from './tipo-logradouro-routing.module';
-import { NbCardModule } from '@nebular/theme';
+import { NbButtonModule, NbCardModule, NbInputModule } from '@nebular/theme';
 import { TipoLogradouroListarComponent } from './listar/tipo-logradouro-listar.component';
 
 @NgModule({
 
   imports: [
     NbCardModule,
+    NbButtonModule,
+    NbInputModule,
     TipoLogradouroRoutingModule
   ],
 
